c96b15683babb27d60aca08794cf629a77fe1286
Travis McKinstry <mckinstrytg@gmail.com>
2020-07-01 13:27:25 -0600

    create restaurant class, create initialize method, create opening_time method, create dishes method, create closing_time method

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -1,37 +1,24 @@
 class Restaurant
-  attr_reader :name, :opening_time, :dishes
 
-  def initialize(opening_time, name)
-    @opening_time = opening_time
+  attr_accessor :opening, :name, :dishes
+
+  def initialize(opening, name)
+    @opening = opening
     @name = name
     @dishes = []
   end
 
-  def add_dish(dish)
-    @dishes << dish
+  def opening_time
+    @opening
   end
 
-  def closing_time(hours)
-    (@opening_time.to_i + hours).to_s + ':00'
+  def dishes
+    @dishes
   end
 
-  def open_for_lunch?
-    @opening_time.to_i <= 11
+  def closing_time(number)
+    closed = @opening.to_i + number
+    return closed.to_s + ':00'
   end
 
-  def menu_dish_names
-    names_upcased = []
-    @dishes.each do |dish|
-      names_upcased << dish.upcase
-    end
-    names_upcased
-  end
-
-  def announce_closing_time(hours)
-    time = closing_time(hours) + "AM"
-    if closing_time(hours).to_i >= 12
-      time = (closing_time(hours).to_i - 12).to_s + ':00PM'
-    end
-    "#{@name} will be closing at #{time}"
-  end
 end
